ec0035c7a4e0b42161b98bf85a4a6f25efb7e52b
juliacsfreitas <freitas.jucsf@gmail.com>
2022-11-16 18:42:38 +0000

    mudanca de figurinhas

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function changeCard() {
-  console.log
+let isIgnite = true
+function changeCard(event) {
+ const card = event.currentTarget
+ const backgroundImage = isIgnite 
+ ? "url(./assets/bg.explorer.svg)" 
+ : "url(./assets/bg.ignite.svg)"
+ isIgnite = !isIgnite
+card.style.backgroundImage = backgroundImage
 }
