9317907aa5f950ae1c65b6063ec3506e24dfeca9
juliacsfreitas <freitas.jucsf@gmail.com>
2022-11-17 14:35:17 +0000

    animação

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let isIgnite = true
+let isIgnite = true; /*é ignite, a 1ºvez é ignite, é verdade*/
 function changeCard(event) {
- const card = event.currentTarget
- const backgroundImage = isIgnite 
- ? "url(./assets/bg.explorer.svg)" 
- : "url(./assets/bg.ignite.svg)"
- isIgnite = !isIgnite
-card.style.backgroundImage = backgroundImage
+  const card = event.currentTarget; /*faz o evento, os dois cliques*/
+  const backgroundImage = isIgnite /*faz a troca das figurinhas*/
+    ? "url(./assets/bg.explorer.svg)" /* (?)entao, entra na funcao e transforma em explorer*/
+    : "url(./assets/bg.ignite.svg)"; /* (:)se nao, muda para ignite*/
+  isIgnite = !isIgnite;
+  /*converte de verdadeiro para falso, se o valor é verdadeiro (isIgnite = true), 
+ (!isIgnite) !nega o valor, sendo assim ele se torna falso*/
+  card.style.backgroundImage = backgroundImage;
 }
